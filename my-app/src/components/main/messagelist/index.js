15f0e23e517cbadce44815c72951f7f34c61cf47
Nikolos1987 <85107815+Nikolos1987@users.noreply.github.com>
2022-05-25 22:04:43 +0700

    final

--- my-app/src/components/main/messagelist/index.js
+++ my-app/src/components/main/messagelist/index.js
@@ -1,11 +1,12 @@
 import Avatar from '@mui/material/Avatar';
-import './stile.css'
+import stile  from './stile.module.css'
+
 export function Messages({data}){
 console.log(data);
   const messageData=data.map((n,index=data.id)=>
-<div key={index} className='messageBlock'>
+<div key={index} className={stile.messageBlock}>
   <Avatar   alt="Remy Sharp" src={`image/${n.image}.jpg`} />
-    <div  className='messageList'>
+    <div  className={stile.messageList}>
         { n.message}</div></div>
   
  )
