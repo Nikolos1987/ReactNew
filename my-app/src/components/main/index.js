61881709f43eebc4d19cc95831d438b939f4a583
Nikolos1987 <85107815+Nikolos1987@users.noreply.github.com>
2022-05-25 21:22:20 +0700

    final

--- my-app/src/components/main/index.js
+++ my-app/src/components/main/index.js
@@ -1,11 +1,12 @@
-import "./Style.css";
+import styles from "./style.module.css";
 import Button from "@mui/material/Button";
 import Input from "@mui/material/Input";
 import { Messages } from "./messagelist";
 import { useEffect, useState,useRef } from "react";
 export function MessageList() {
 
-
+const style=styles
+console.log(style.header);
   const[input,setInput]=useState('')
   const[send,setSend]=useState([])
   const add=()=>{setSend((a)=>[...a,{message:input,name:"user",image:2,id:Date.now()}])}
@@ -16,11 +17,11 @@ useEffect(()=>{if(send.length>0&&send[send.length-1].name==="user"){
 const ref=useRef()
 useEffect(()=>{ref.current?.focus()},[])
   return (
-    <div className="wrapper">
-        <div className="header"><h1>Super Messanger</h1></div>
-        <div className="main" ><Messages  data={send}/></div>
-      <div className="message">
-        <Input className="btn" ref={ref} onChange={(e)=>setInput(e.target.value)} fullWidth={true}></Input>
+    <div className={style.wrapper}>
+        <div className={style.header}><h1>Super Messanger</h1></div>
+        <div className={style.main} ><Messages  data={send}/></div>
+      <div className={style.message}>
+        <Input className={style.btn} ref={ref} onChange={(e)=>setInput(e.target.value)} fullWidth={true}></Input>
         <Button onClick={add} variant="text" color={"secondary"}>
           отправить
         </Button>
